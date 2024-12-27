c2d14ed8f41d1488c7d2a6c28261b78a7ada3db6
muhammed-jamiu <anatemuhammed@gmail.com>
2024-12-27 19:01:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+const deg = 6;
+
+setInterval(() => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
+
+  hr.style.transform = `rotate(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotate(${mm}deg)`;
+  sc.style.transform = `rotate(${ss}deg)`;
+});
